b064c22f8028f5333202fe31de64bfa49372f0ca
JovanB369 <bjovan369@gmail.com>
2023-01-28 20:19:13 +0100

    add random color and did 10% opacity

--- app.js
+++ app.js
@@ -5,6 +5,15 @@
 const container = document.querySelector('.container')
 const resetButton =  document.querySelector('button')
 
+const createRandomRGB = () =>{
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return { r, g, b}
+
+}
+
 const createGrid = (amtOfGrids) =>{
 
 const wrapper = document.createElement('div')
@@ -15,13 +24,22 @@ wrapper.classList.add('wrapper')
         row.classList.add('grid-row')
 
         for (let j = 0;j < amtOfGrids;j++){
+            const { r, g, b} = createRandomRGB()
             const widthAndHeight  = 900 / amtOfGrids
             const gridBox = document.createElement('div')
             gridBox.style.width = `${widthAndHeight}px`
             gridBox.style.height = `${widthAndHeight}px`
 
             gridBox.addEventListener('mouseenter', () => {
-                gridBox.style.backgroundColor = 'black'
+                const currenOpacity = gridBox.style.opacity
+                const bgColor = col = "rgba(" + r + "," + g + "," + b + ")" //> random color
+                gridBox.style.background = bgColor
+                // add 10% opacity each time you click on box
+                if (currenOpacity){
+                    gridBox.style.opacity = Number(currenOpacity) + .1
+                }else{
+                    gridBox.style.opacity = .1
+                }
             })
 
             gridBox.classList.add('grid-box')
